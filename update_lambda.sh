4dbb8efad6f57f841851fbd6a56650365f48873b
Ellery Kreloff <kreloff@Ellerys-MacBook-Pro.local>
2019-11-13 21:09:19 -0500

    adding font support

--- update_lambda.sh
+++ update_lambda.sh
@@ -1,7 +1,7 @@
 cd PIL
 zip -r9 /Users/kreloff/upload-artist-image/function.zip . 
 cd /Users/kreloff/upload-artist-image/
-zip -g function.zip lambda_function.py constants.py 
+zip -g function.zip lambda_function.py raleway .
 echo "Publishing lambda version..."
 aws lambda publish-version --function-name upload-artist-image 
 echo "Updating function code..."
