654542d1bf4b35c8c7b342d3efcb75c239284bb3
Ellery Kreloff <ellery.kreloff@gmail.com>
2019-12-04 19:41:33 -0500

    added everything back

---
+++ update_lambda.sh
@@ -0,0 +1,13 @@
+cd PIL
+zip -r9 ../function.zip *
+cd ../fonts
+zip -r9 ../function.zip *
+cd ../numpy
+zip -r9 ../function.zip *
+cd ../
+zip -g function.zip lambda_function.py util.py transforms.py
+echo "Publishing lambda version..."
+aws lambda publish-version --function-name upload-artist-image 
+echo "Updating function code..."
+aws lambda update-function-code --function-name upload-artist-image --zip-file fileb://function.zip 
+echo "Update complete!"
